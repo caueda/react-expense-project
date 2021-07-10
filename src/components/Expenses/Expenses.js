78d930fda837bc077e7ad7be77dec765897069c1
carlos.ueda <carlos.ueda@gmail.com>
2021-07-10 06:51:00 -0400

    Creating dynamic content.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,11 +7,21 @@ import {useState} from "react";
 
 function Expenses(props) {
     const [yearSelected, setYearSelected] = useState('2019');
+    const filteredExpense = props.expenses.filter(expense => expense.date.getFullYear().toString() === yearSelected);
+    const getContent = () => {
+        if(filteredExpense.length === 0)
+            return <p>No expenses found.</p>;
+        else
+            return filteredExpense.map(expense => <ExpenseItem key={expense.id} expenseData={expense}/>);
+    }
+
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter filteredYear={yearSelected} onChangeFilter={setYearSelected}/>
-                {props.expenses.filter(expense => expense.date.getFullYear() === Number(yearSelected)).map(expense => <ExpenseItem expenseData={expense}/>)}
+                <ExpensesFilter
+                    filteredYear={yearSelected}
+                    onChangeFilter={setYearSelected}/>
+                {getContent()}
             </Card>
         </div>
     );
